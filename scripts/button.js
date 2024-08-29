ed242f01d081b4e259f3d286b27f6719bc5e523c
NoitaminAoki <s2.DanielAoki@gmail.com>
2024-08-29 18:27:21 +0700

    Fixed errors with anuke not allowing ui lib exist

--- scripts/button.js
+++ scripts/button.js
@@ -15,7 +15,7 @@
 	along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
-const ui = require("ui-lib/library");
+const ui = require("ui_lib/library");
 
 // Android also doesn't load the logger automatically in 6.0
 Vars.loadLogger();
